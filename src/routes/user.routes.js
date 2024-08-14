d3dcc44a5d43dfee29463378138effd7faa5ef9e
shrutirthr29 <shray.shruti@gmail.com>
2024-08-14 13:46:07 +0530

    fix: logout user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,16 @@
 import {Router} from "express"
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js"
+import { loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    registerUser, 
+    changeCurrentPassword, 
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    getUserChannelProfile,
+    getWatchHistory
+
+} from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 
